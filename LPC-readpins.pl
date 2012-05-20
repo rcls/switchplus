ed12976f7bcdbe5fd1258c2613aaa6d51cd07c38
Ralph Loader <suckfish@ihug.co.nz>
2012-05-20 13:08:46 +1200

    Pin aggregations.

--- LPC-readpins.pl
+++ LPC-readpins.pl
@@ -18,9 +18,42 @@ my $type = qr{(?: +(?:\bI/O\b|\b[IO]\b|-))?};
 my $func = qr/[A-Z0-9_]+(?:\[\d+\])?\s*--/i;
 
 my $in_table3;
-my $item_pins;
+my $pin_item;
 
 'CLK0' =~ /^$symbol$/  or  die;
+
+sub pin_start($$$$$)
+{
+    $pin_item = {
+        symbol => $_[0],
+        pins => $_[1],
+        reset => $_[2],
+        type => $_[3],
+        func => $_[4]
+    }
+}
+
+sub pin_eject()
+{
+    return  unless  $pin_item;
+    $pin_item->{pins} = join '/', @{$pin_item->{pins}};
+    print
+        join ' ',
+        map { uc($_) . "=$pin_item->{$_}"} qw/symbol pins reset type func/;
+    print "\n";
+    $pin_item = undef;
+}
+
+sub pin_pins(@)
+{
+    my @p = @_;
+
+    for (@{$pin_item->{pins}}) {
+        last  unless  @p;
+        $_ .= shift @p  if  /,$/  or  /\d{3}/  and  $p[0] =~ /^,/;
+    }
+}
+
 
 while (<>) {
     $in_table3 = 1  if  /^Table 3\b/;
@@ -36,13 +69,14 @@ while (<>) {
         my $f = $5;
 
         $p =~ s/^ +//;
-        $p =~ s/ +$//;
-        $p =~ s, +,/,g;
+        #$p =~ s/ +$//;
+        $p = [ split / +/, $p ];
+        #$p =~ s, +,/,g;
         $r =~ s/ +//g;
         $t =~ s/^ +//;
 
-        print "SYMBOL=$s PINS=$p RESET=$r TYPE=$t FUNC=$f\n";
-        $item_pins = 1;
+        pin_eject;
+        pin_start $s, $p, $r, $t, $f;
         next;
     }
 
@@ -51,18 +85,19 @@ while (<>) {
         my $t = $2;
         my $f = $3;
         $t =~ s/^ +//;
+        pin_eject;
         print " RESET2=$r TYPE=$t FUNC=$f\n";
-        $item_pins = undef;
         next;
     }
 
-    if ($item_pins  and  /^ {9,}((?: +$pin)+)/) {
+    if ($pin_item  and  /^ {9,}((?: +$pin)+)/) {
         my $p = $1;
         $p =~ s/^ +//;
-        $p =~ s, +,/,g;
-        print " PINS=$p\n";
+        pin_pins split / +/, $p;
         next;
     }
 
-    $item_pins = undef;
+    next  if  /^$/;
+
+    pin_eject;
 }
