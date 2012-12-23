0951cc0a6d89894e43536bdaedf527fe33d1f6cf
Ralph Loader <suckfish@ihug.co.nz>
2012-12-24 07:55:22 +1300

    Factor out the output serdes. Don't pretend hdmi out is 3 state

--- tmds/tmds_encode.vhd
+++ tmds/tmds_encode.vhd
@@ -59,17 +59,21 @@ use ieee.numeric_std.all;
 library work;
 use work.defs.all;
 
+library unisim;
+use unisim.vcomponents.all;
+
 entity tmds_channel is
   port (D : in byte_t;
         C0, C1 : in std_logic;
         DE : in std_logic;
-        Q : out unsigned(3 downto 0);
-        clk : in std_logic;
-        clk_nibble : in std_logic);
+        Qp, Qn : out std_logic;
+        clk, clk_nibble, ioclk, lock, serdesstrobe : in std_logic);
 end tmds_channel;
 architecture tmds_channel of tmds_channel is
   signal buf, wordA, wordB, bufA, bufB : dec_t;
   signal which, whichB, whichC : std_logic := '0';
+  signal nibble : nibble_t;
+  signal Q : std_logic;
   signal phase : integer range 0 to 4 := 0;
 begin
   encode : entity work.tmds_encode_byte port map (D, C0, C1, DE, buf, clk);
@@ -97,13 +101,24 @@ begin
       phase <= phase + 1;
     end if;
     case phase is
-      when 0 => Q <= wordA(3 downto 0);
-      when 1 => Q <= wordA(7 downto 4);
-      when 2 => Q <= wordB(1 downto 0) & wordA(9 downto 8);
-      when 3 => Q <= wordB(5 downto 2);
-      when 4 => Q <= wordB(9 downto 6);
+      when 0 => nibble <= wordA(3 downto 0);
+      when 1 => nibble <= wordA(7 downto 4);
+      when 2 => nibble <= wordB(1 downto 0) & wordA(9 downto 8);
+      when 3 => nibble <= wordB(5 downto 2);
+      when 4 => nibble <= wordB(9 downto 6);
     end case;
   end process;
+
+  osd : oserdes2
+    generic map (DATA_WIDTH=> 4, DATA_RATE_OQ=> "SDR", DATA_RATE_OT=> "SDR")
+    port map (D1=>nibble(0), D2=> nibble(1), D3=> nibble(2), D4=>nibble(3),
+              T1=>'0', T2=>'0', T3=>'0', T4=>'0',
+              SHIFTIN1=>'0', SHIFTIN2=>'0', SHIFTIN3=>'0', SHIFTIN4=>'0',
+              CLK0=> ioclk, CLK1=> '0', CLKDIV=> clk_nibble,
+              IOCE=> serdesstrobe,
+              RST=> '0', TCE=> '1', TRAIN=>'0',
+              OQ=> Q);
+  obd : obufds port map (I=> Q, O=> Qp, OB=> Qn);
 end tmds_channel;
 
 library ieee;
@@ -128,54 +143,21 @@ architecture tmds_encode of tmds_encode is
   signal ioclk, lock, serdesstrobe : std_logic;
   signal clk180 : std_logic;
   signal Rnibble, Gnibble, Bnibble : nibble_t;
-  signal Rq, Rt, Gq, Gt, Bq, Bt, Cq : std_logic;
+  signal Rq, Gq, Bq, Cq : std_logic;
   attribute keep_hierarchy of tmds_encode : architecture is "soft";
 begin
   Rc : entity work.tmds_channel port map (
-    R, ctl2, ctl3, DE, Rnibble, clk, clk_nibble);
+    R, ctl2, ctl3, DE, Rp, Rn, clk, clk_nibble, ioclk, lock, serdesstrobe);
   Gc : entity work.tmds_channel port map (
-    G, ctl1, ctl0, DE, Gnibble, clk, clk_nibble);
+    G, ctl1, ctl0, DE, Gp, Gn, clk, clk_nibble, ioclk, lock, serdesstrobe);
   Bc : entity work.tmds_channel port map (
-    B, hsync, vsync, DE, Bnibble, clk, clk_nibble);
+    B, hsync, vsync, DE, Bp, Bn, clk, clk_nibble, ioclk, lock, serdesstrobe);
 
-  Ro : oserdes2
-    generic map (DATA_WIDTH=> 4, DATA_RATE_OQ=> "SDR", DATA_RATE_OT=> "SDR")
-    port map (D1=>Rnibble(0), D2=> Rnibble(1), D3=> Rnibble(2), D4=>Rnibble(3),
-              T1=>'0', T2=>'0', T3=>'0', T4=>'0',
-              SHIFTIN1=>'0', SHIFTIN2=>'0', SHIFTIN3=>'0', SHIFTIN4=>'0',
-              CLK0=> ioclk, CLK1=> '0', CLKDIV=> clk_nibble,
-              IOCE=> serdesstrobe, RST=> '0',
-              TCE=> '1', TRAIN=>'0',
-              -- OCE=>'0', TCE=>'0',
-              OQ=> Rq, TQ=> Rt);
-  Go : oserdes2
-    generic map (DATA_WIDTH=> 4, DATA_RATE_OQ=> "SDR", DATA_RATE_OT=> "SDR")
-    port map (D1=>Gnibble(0), D2=> Gnibble(1), D3=> Gnibble(2), D4=>Gnibble(3),
-              T1=>'0', T2=>'0', T3=>'0', T4=>'0',
-              SHIFTIN1=>'0', SHIFTIN2=>'0', SHIFTIN3=>'0', SHIFTIN4=>'0',
-              CLK0=> ioclk, CLK1=> '0', CLKDIV=> clk_nibble,
-              IOCE=> serdesstrobe, RST=> '0',
-              TCE=> '1', TRAIN=>'0',
-              -- OCE=>1, TCE=>1,
-              OQ=> Gq, TQ=> Gt);
-  Bo : oserdes2
-    generic map (DATA_WIDTH=> 4, DATA_RATE_OQ=> "SDR", DATA_RATE_OT=> "SDR")
-    port map (D1=>Bnibble(0), D2=> Bnibble(1), D3=> Bnibble(2), D4=>Bnibble(3),
-              T1=>'0', T2=>'0', T3=>'0', T4=>'0',
-              SHIFTIN1=>'0', SHIFTIN2=>'0', SHIFTIN3=>'0', SHIFTIN4=>'0',
-              CLK0=> ioclk, CLK1=> '0', CLKDIV=> clk_nibble,
-              IOCE=> serdesstrobe, RST=> '0',
-              TCE=> '1', TRAIN=>'0',
-              -- OCE=>1, TCE=>1,
-              OQ=> Bq, TQ=> Bt);
   clk180 <= not clk;
   --clk180 <= clk;
   Co : oddr2
     port map (D0=>'1', D1=>'0', CE=>'1', Q=> Cq, C0=>clk, C1=>clk180);
-  Rd : obuftds port map (I=> Rq, T=> Rt, O=> Rp, OB=> Rn);
-  Gd : obuftds port map (I=> Gq, T=> Gt, O=> Gp, OB=> Gn);
-  Bd : obuftds port map (I=> Bq, T=> Bt, O=> Bp, OB=> Bn);
-  Cd : obufds port map (I=>Cq, O=> Cp, OB=>Cn);
+  Cd : obufds port map (I=> Cq, O=> Cp, OB=> Cn);
   bpl : bufpll generic map (DIVIDE=>4)
     port map (GCLK=> clk_nibble, PLLIN=> clk_bit, LOCKED=> locked,
               IOCLK=> ioclk, SERDESSTROBE=>serdesstrobe, LOCK=> lock);
